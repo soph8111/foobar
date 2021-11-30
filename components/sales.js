87bee8150043e92bcf1e30297de8bc964e9c5ea7
Caroline <caro713f@stud.kea.dk>
2021-11-30 17:49:21 +0100

    changes to sales

--- components/sales.js
+++ components/sales.js
@@ -2,13 +2,14 @@
 
 export function showSales(foobarData){
     document.querySelector("#customer_amount").textContent = calculateCustomers(foobarData);
-    document.querySelector("#beers_amount").textContent = calculateBeers(foobarData);
+    document.querySelector("#beers_amount").textContent = customerCount.beerCount;
     document.querySelector("#money_amount").textContent = calculateMoney();
 }
 
-// Tomt array til at tælle kunder
+// Tomt array til at tælle kunder og øl
 const customerCount = {
     customerArray: [],
+    beerCount: 0,
 };
 
   function calculateCustomers(customers){
@@ -18,34 +19,20 @@ const customerCount = {
         if(!customerCount.customerArray.includes(customer.id)){
             // Pusher customer id'et til arrayet, så vi kan tælle længden på arrayet og få antallet af kunder
             customerCount.customerArray.push(customer.id);
-        }   
+            // Sætter tælleren på øl til at være lig tallet før + længden af ordrerne
+            customerCount.beerCount = customerCount.beerCount + customer.order.length;
+        }
     });
     // Returnerer længden af arrayet, som så bliver udskrevet i showSales
     return customerCount.customerArray.length;
 }
 
-// Tomt array til at tælle øl
-const beerCount = {
-    beerArray: [],
-};
-
-function calculateBeers(customers){
-
-    // console.log(customers);
-    customers.forEach((customer) => {
-        //Sørger for kun at tælle customer 1 gang - hvis samme kunde stadig bliver serviceret når det fetches igen)
-        if (!beerCount.beerArray.includes(customer.id)){
-            // Pusher ordren til arrayet vha. split operator (så vi ikke får et array i et array)
-            beerCount.beerArray.push(...customer.order);
-        }
-    });
-    
-    // Returnerer længden af arrayet og udskriver antallet af solgte øl i showSales
-    return beerCount.beerArray.length;
-}
+// function calculateBeers(){
+//     return customerCount.beerCount;
+// }
 
 
 function calculateMoney(){
-    //Ganger arrayets lægde (antallet af øl) med 50 (pris pr øl)
-    return beerCount.beerArray.length * 50;
+    // Ganger antallet af øl med 50 (pris pr øl)
+    return customerCount.beerCount * 50;
 }
