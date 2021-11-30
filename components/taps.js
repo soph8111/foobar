7372f8454b73966e25bd6e7b0ef6f711d83c2d4e
Caroline <caro713f@stud.kea.dk>
2021-11-30 11:05:38 +0100

    taps works

---
+++ components/taps.js
@@ -0,0 +1,47 @@
+"use strict";
+
+export function showTaps(foobarData, beerData) {
+    console.log(foobarData);
+    console.log(beerData);
+    const tapsHolder = document.querySelector("#taps_wrapper");
+    const template = document.querySelector(".taps_content").content;
+    tapsHolder.textContent = "";
+    foobarData.forEach((tap) => {
+      const klon = template.cloneNode(true);
+      klon.querySelector(".tap_no").textContent = tap.id+1;
+    //   klon.querySelector(".staff_img").src = `./images/${bartenders.name}.png`;
+      klon.querySelector(".beer_name").textContent = tap.beer;
+      klon.querySelector(".beer_name").textContent = getPercentage(tap);
+      klon.querySelector(".alcohol").textContent = getAlcohol(beerData, tap);
+      klon.querySelector(".beer_type").textContent = getType(beerData, tap);
+      tapsHolder.appendChild(klon);
+    });
+    
+}
+
+function getAlcohol(beerData, tap){
+    let alcohol;
+    beerData.forEach((beer) => {
+        if(tap.beer === beer.name){
+            alcohol = beer.alc;    
+        }     
+    })
+
+    return alcohol;    
+}
+
+function getType(beerData, tap){
+    let type;
+    beerData.forEach((beer) => {
+        if(tap.beer === beer.name){
+            type = beer.category;    
+        }     
+    })
+
+    return type;    
+}
+ 
+function getPercentage(tap){
+    console.log(tap.level);
+    return (100 * tap.level) / tap.capacity;
+}
--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import { showStaff } from "./components/staff.js";
 import { showOrders } from "./components/queue.js";
 import {showStorage} from "./components/storage.js";
 import {showSales} from "./components/sales.js";
+import {showTaps} from "./components/taps.js";
 
 window.addEventListener("DOMContentLoaded", start);
 
@@ -17,19 +18,34 @@ function start() {
   setInterval(loadJSON, 50000);
 }
 
-let foobarUrl = "https://the-three-must-get-beers.herokuapp.com/";
-async function loadJSON() {
-  //   console.log("laodJSON");
-  const response = await fetch(foobarUrl);
-  const foobarData = await response.json();
-  // console.log("foobarData", foobarData);
+// let foobarUrl = "https://the-three-must-get-beers.herokuapp.com/";
+// async function loadJSON() {
+//   //   console.log("laodJSON");
+//   const response = await fetch(foobarUrl);
+//   const foobarData = await response.json();
+//   // console.log("foobarData", foobarData);
 
-  showOrders(foobarData.queue);
+//   showOrders(foobarData.queue);
+//   // showFav(foobarData.serving);
+//   showStaff(foobarData.bartenders);
+//   showTaps(foobarData.taps);
+//   showStorage(foobarData.storage);
+//   showSales(foobarData.serving);
+// }
+
+
+async function loadJSON(){
+  await Promise.all([fetch("https://the-three-must-get-beers.herokuapp.com/")
+      .then((res) => res.json()), fetch("https://the-three-must-get-beers.herokuapp.com/beertypes")
+      .then((res) => res.json())])
+      .then((foobarData) => {
+          // When loaded, prepare data objects
+          showOrders(foobarData[0].queue);
   // showFav(foobarData.serving);
-  showStaff(foobarData.bartenders);
-  // showTaps(foobarData.taps);
-  showStorage(foobarData.storage);
-  showSales(foobarData.serving);
+  showStaff(foobarData[0].bartenders);
+  showTaps(foobarData[0].taps, foobarData[1]);
+  showStorage(foobarData[0].storage);
+  showSales(foobarData[0].serving);  
+      });
+
 }
-
-
