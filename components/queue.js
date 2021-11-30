f838cbf227c2f4c096ec7b5fbbbb632daa954a3c
Sophie Spang <soph8111@stud.kea.dk>
2021-11-30 12:16:25 +0100

    queue list on orders works, but without ex. 2x beer

--- components/queue.js
+++ components/queue.js
@@ -1,21 +1,32 @@
 "use strict";
-export function showOrders(foobarData){
-    // console.log(foobarData);
-    document.querySelector(".queue").textContent = `In queue: ${foobarData.length}`;
-    const ordersHolder = document.querySelector("#orders_wrapper");
-    const template = document.querySelector(".orders_content").content;
-    ordersHolder.textContent = "";
-    foobarData.forEach((order) => {
-      const klon = template.cloneNode(true);
-      // console.log(order);
-      klon.querySelector(".order_no").textContent = order.id;
-      klon.querySelector(".order_beers").textContent = order.order;
-      ordersHolder.appendChild(klon);
-    });
+
+export function showOrders(foobarData) {
+  document.querySelector(".queue").textContent = `In queue: ${foobarData.length}`;
+  const ordersHolder = document.querySelector("#orders_wrapper");
+  const template = document.querySelector(".orders_content").content;
+  ordersHolder.textContent = "";
+  foobarData.forEach((order) => {
+    const klon = template.cloneNode(true);
+    klon.querySelector(".order_no").textContent = order.id;
+    const list = createBeerList(order.order);
+    klon.querySelector(".order_beers").append(list);
+
+    ordersHolder.appendChild(klon);
+  });
 }
 
+function createBeerList(beersOnOrder) {
+  let list = document.createElement("ul");
+
+  beersOnOrder.forEach((beer) => {
+    const li = document.createElement("li");
+    li.append(beer);
+    list.appendChild(li);
+  });
+
+  return list;
+}
 
 // Vise order på en liste li
 
 // Sætte 1x, 2x osv foran ordren alt efter antal af samme slags øl
-
