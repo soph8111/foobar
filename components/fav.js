fbbdea75891b21ce96ae252c28397fa634f4d2f9
Caroline <caro713f@stud.kea.dk>
2021-12-01 10:52:40 +0100

    favourites worksgit add --all

--- components/fav.js
+++ components/fav.js
@@ -1,122 +1,5 @@
 "use strict";
 
-export function showFav(foobarData){
-
-    calculateBeers(foobarData);
-    
-}
-
-// Tomt array til at tælle øl
-
-// const beerArray = [];
-
-// const beerTypes = [
-    // {name: githop, amount: 0},
-    // {elhefe: 0},
-    // {steampunk: 0},
-// ]
-// const beerTypes = {
-//     elHefe: 0,
-//     fairyTaleAle: 0,
-//     row26: 0,
-//     steampunk: 0,
-//     gitHop: 0,
-//     sleighRide: 0,
-//     hollabackLager: 0,
-//     hoppilyEverAfter: 0,
-//     mowinTime: 0,
-//     ruinedChildhood: 0,
-// }
-
-// function calculateBeers(beers){
-
-//     beers.forEach((beer) => {
-//         console.log(beer.order);
-//         //Sørger for kun at tælle customer 1 gang - hvis samme kunde stadig bliver serviceret når det fetches igen)
-//         if (!beerArray.includes(beer.id)){
-//             // Pusher ordren til arrayet vha. split operator (så vi ikke får et array i et array)
-//             beerArray.push(...beer.order);
-//         }
-//     });
-//     console.log(beerArray);
-//     const gitHop = beerArray.filter(beer => beer.includes("GitHop"));
-//     beerTypes.push(gitHop);
-//     const elHefe = beerArray.filter(beer => beer.includes("El Hefe"));
-//     beerTypes.push(elHefe);
-//     const steampunk = beerArray.filter(beer => beer.includes("Steampunk"));
-//     beerTypes.push(steampunk);
-//     // console.log(beerTypes.gitHop.length);
-//     console.log(beerTypes);
-//     // beerTypes.sort(setSort);
-// }
-
-// function setSort(a, b){
-//     if(a.elements.length > b.elements.length) {
-//         return -1;
-//     } else {
-//         return 1;
-//     }
-// }
-
-
-//TEST 2
-// const allBeers = [];
-// const count = {}
-
-// function calculateFav(foobarData){
-// //     foobarData.forEach((order) => {
-// //         // console.log(order.order);
-// //         //Sørger for kun at tælle customer 1 gang - hvis samme kunde stadig bliver serviceret når det fetches igen)
-// //         if (!allBeers.includes(order.id)){
-// //             // Pusher ordren til arrayet vha. split operator (så vi ikke får et array i et array)
-// //             allBeers.push(order.order);
-// //         }
-// //         getOccurrence(...allBeers);
-// //     });
-// //   console.log(allBeers);
-// }
-
-// function getOccurrence(allBeers) {
-
-// const gitHop = allBeers.filter(beer1 => beer1.includes("GitHop"));
-// //   console.log(gitHop);
-// }
-
-
-// TEST 1
-
-// const allBeers = {
-//     elHefe: 0,
-//     fairyTaleAle: 0,
-//     row26: 0,
-//     steampunk: 0,
-//     gitHop: 0,
-//     sleighRide: 0,
-//     hollabackLager: 0,
-//     hoppilyEverAfter: 0,
-//     mowinTime: 0,
-//     ruinedChildhood: 0,
-// }
-// function calculateFav(data){   
-//     if(!data.includes(data.id)){
-//         data.forEach((order) => {
-//             console.log(order.order);
-//             if(order.order.includes("GitHop")){
-//                 allBeers.gitHop+= order.order.filter(order => order == "GitHop").length;
-//             };
-            
-//         });
-//     }
-    
-//     console.log(allBeers.gitHop);
-// }
-
-
-
-
-
-// TEST 3
-
 const orderArray = [];
 
 const beerCount = [
@@ -134,15 +17,23 @@ const beerCount = [
 
  const sorted = beerCount.slice();
 
+export function showFav(foobarData){
+
+    calculateBeers(foobarData);
+    document.querySelector("#number1").textContent = sorted[0].name;
+    document.querySelector("#number2").textContent = sorted[1].name;
+    document.querySelector("#number3").textContent = sorted[2].name;
+}
+
 function calculateBeers(foobarData){
     
     foobarData.forEach((order) => {
             
-        //Sørger for kun at tælle customer 1 gang - hvis samme kunde stadig bliver serviceret når det fetches igen)
+        //Sørger for kun at tælle customer 1 gang - hvis samme kunde stadig bliver serviceret når det fetches igen
         if(!orderArray.includes(order.id)){
-            // Pusher customer id'et til arrayet, så vi kan tælle længden på arrayet og få antallet af kunder
+            // Pusher customer id'et til arrayet, så vi kan tjekke at ordren ikke har været kørt før
             orderArray.push(order.id);
-            // Sætter tælleren på øl til at være lig tallet før + længden af ordrerne
+            // Sætter tælleren på øllen til at være lig tallet før + længden af array med samme øl i
             console.log(order.order);
             if(order.order.includes("GitHop")){
                 beerCount[0].amount+= order.order.filter(order => order == "GitHop").length;
@@ -175,14 +66,17 @@ function calculateBeers(foobarData){
                 beerCount[9].amount+= order.order.filter(order => order == "Ruined Childhood").length;
             };
         }
-        console.log(beerCount);
-        console.log(sorted);
+        // console.log(beerCount);
+        // console.log(sorted);
+        // Kalder functionen sort by
         sortBy();
+
     });
 
 
 }
 
+// Sorterer efter amount, så den med flest ligger øverst i arrayet
 function sortBy(){
     sorted.sort((a, b) => {
         return b.amount - a.amount;
